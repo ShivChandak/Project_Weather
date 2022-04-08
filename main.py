f952fa56bc98580442c6ff7664555f40c49affa3
ShivChandak <94048313+ShivChandak@users.noreply.github.com>
2022-04-08 15:40:37 +0530

    Updating to flask application

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-#Program to fetch real time weather and temperature in degree celcius from openweathermap.org using API.
+from flask import Flask, render_template, request
 import requests
-import sys
 
-#Get your API key from https://openweathermap.org/api to get access to the data.
-api_key = input("Enter your API key: ")
-API_KEY = api_key
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+app = Flask(__name__)
 
-#Getting location to show weather for.
-city = input("Enter City: ")
-request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
-response = requests.get(request_url)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-#If the request is satisfied with code 200 (ok) then do the necessary. 'data' gets all the data from the json file.
-#data is trimmed to show what is required.
-if response.status_code == 200:
-    data = response.json()
-    weather = data['weather'][0]['description']
-    print("Weather:", weather.capitalize())
-    temperature = round(data["main"]["temp"] - 273.15, 2)
-    print(f"Temperature: " + str(temperature) + "\N{DEGREE SIGN}" + " C")
-    sys.exit(0)
-print("An error occured!")
-sys.exit(1)
+@app.route("/result")
+def result():
+    API_KEY = request.form.get("api_key")
+    BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+    city = request.form.get("city")
+    request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+    response = requests.get(request_url)
+    if response.status_code == 200:
+        data = response.json()
+        weather = data['weather'][0]['description']
+        x = f'Weather: {weather.capitalize()}'
+        temperature = round(data["main"]["temp"] - 273.15, 2)
+        y = f'Temperature: {str(temperature)} \N{DEGREE SIGN}C'
+        answer = [x , y]
+        return render_template("result.html", ans=answer)
+    else:
+        return render_template("error.html")
